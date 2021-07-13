4656fc5a8108ac5f907c05ea6711f04e4df5a874
Solomon Irving <solomon.irving@gmail.com>
2021-07-13 16:54:08 -0700

    7/13 FINAL

--- src/components/Algolia.js
+++ src/components/Algolia.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import algoliasearch from 'algoliasearch';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+// import React, { Component } from 'react';
+// import algoliasearch from 'algoliasearch';
+// import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 
-const searchClient = algoliasearch('VQI5TOVS3P', 'c303f0377410aa3d69834c9fafcfd7c4');
+// const searchClient = algoliasearch('VQI5TOVS3P', 'c303f0377410aa3d69834c9fafcfd7c4');
 
 
-export default class Algolia extends Component {
-    render() {
-        return (
-            <InstantSearch searchClient={searchClient} indexName="demo_ecommerce">
-            <SearchBox />
-            <Hits />
-            </InstantSearch>
-        )
-    }
-}
+// export default class Algolia extends Component {
+//     render() {
+//         return (
+//             <InstantSearch searchClient={searchClient} indexName="demo_ecommerce">
+//             <SearchBox />
+//             <Hits />
+//             </InstantSearch>
+//         )
+//     }
+// }
